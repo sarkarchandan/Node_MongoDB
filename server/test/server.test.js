2ac1561d1b0a19b84fdad05c3fcf92fce1cb67dd
chandan <elitegreen@gmail.com>
2018-05-27 11:24:31 +0200

    Added tests for GET/todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,10 +4,22 @@ const expect = require("expect");
 const {app} = require("./../server");
 const {Todo} = require("./../model/Todo-model");
 
+const todos = [
+  {
+    text: "Play Detroit Become Human"
+  },
+  {
+    text: "Play Horizon Zero Dawn"
+  }
+];
+
 beforeEach((done) => {
   Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => {
     done();
-  }).catch((error) => {
+  })
+  .catch((error) => {
     done(error);
   })
 });
@@ -30,7 +42,11 @@ describe("POST /todos", () => {
         return done(error);
       }
 
-      Todo.find().then((todos) => {
+      Todo.find(
+        {
+          text
+        }
+      ).then((todos) => {
         expect(todos.length).toBe(1)
         expect(todos[0].text).toBe(text)
         done();
@@ -54,13 +70,27 @@ describe("POST /todos", () => {
       }
 
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((error) => {
         done(error);
       });
     });
   });
-  
+
+});
+
+describe("GET /todos", () => {
+
+  it("should get all todos", (done) => {
+
+    supertest(app)
+    .get("/todos")
+    .expect(200)
+    .expect((response) => {
+      expect(response.body.todos.length).toBe(2);
+    }).end(done);
+
+  });
 
 });
